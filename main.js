238fce7f790d5f13412a215ae51a176a3b70810f
Duncan Ritchie <duncanritchie@btopenworld.com>
2019-09-04 16:02:15 +0100

    Snake moves around board

--- main.js
+++ main.js
@@ -43,6 +43,7 @@ let direction = "right";
 const foodDiv = document.getElementById("food");
 const snakeHeadDiv = document.getElementById("snake-head");
 const snakeJointDivs = document.getElementsByClassName("snake-joint");
+const startButton = document.getElementById("start-button");
 
 
 
@@ -64,9 +65,87 @@ const placeFood = () => {
     foodDiv.style.top = (y * cellSize)+"px";
 }
 
+const placeSnake = () => {
+    snakeHeadDiv.style.left = (snake[0].x * cellSize)+"px";
+    snakeHeadDiv.style.top = (snake[0].y * cellSize)+"px";
+    for (let i = 0; i < snakeJointDivs.length; i++) {
+        snakeJointDivs[i].style.left = (snake[i+1].x * cellSize) + "px";
+        snakeJointDivs[i].style.top = (snake[i+1].y * cellSize) + "px";
+    }
+}
+
+const moveSnake = () => {
+    for (let i = snake.length-1; i > 0; i--) {
+        snake[i].x = snake[i-1].x;
+        snake[i].y = snake[i-1].y;
+    }
+    switch (direction) {
+        case "up":
+            if (snake[0].y == 0) {
+                snake[0].y = boardHeight-1;
+            }
+            else {
+                snake[0].y--;
+            }
+            break;
+        case "down":
+            if (snake[0].y == boardHeight-1) {
+                snake[0].y = 0;
+            }
+            else {
+                snake[0].y++;
+            }
+            break;
+        case "left":
+            if (snake[0].x == 0) {
+                snake[0].x = boardWidth-1;
+            }
+            else {
+                snake[0].x--;
+            }
+            break;
+        case "right":
+            if (snake[0].x == boardWidth-1) {
+                snake[0].x = 0;
+            }
+            else {
+                snake[0].x++;
+            }
+    }
+    placeSnake();
+}
+
+
+const changeDirection = (e) => {
+    let oldDirection = direction;
+    switch (e.key.toLowerCase()) {
+        case "w": 
+            if (direction != "down") direction = "up";
+            break;
+        case "a":
+            if (direction != "right") direction = "left";
+            break;
+        case "s":
+            if (direction != "up") direction = "down";
+            break;
+        case "d":
+            if (direction != "left") direction = "right";
+    }
+    if (oldDirection != direction) {
+        console.log(direction);
+    }
+}
+
+const startGame = () => {
+    document.addEventListener("keydown",changeDirection);
+    startButton.remove();
+    const interval = setInterval(moveSnake,1000)
+}
 
 ////
 //// GAME START
 ////
 
 placeFood();
+
+startButton.addEventListener("click",startGame);
