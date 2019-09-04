777e7c1f19339fd60bdc6dbe9595e70221acf72a
Duncan Ritchie <duncanritchie@btopenworld.com>
2019-09-04 15:12:52 +0100

    Food & snake appear as at game-start

--- main.js
+++ main.js
@@ -1,1 +1,72 @@
 console.log("JavaScript loaded!")
+
+
+
+////
+//// GLOBAL CONSTANTS
+////
+
+const cellSize = 20; // in pixels
+const boardWidth = 17; // in cells
+const boardHeight = 17; // in cells
+
+
+
+////
+//// GAMEPLAY VARIABLES
+////
+
+let food = {
+    x: 5,
+    y: 5
+}
+
+let snake = [
+    {
+        x: 2,
+        y: 2
+    },
+    {
+        x: 1,
+        y: 2
+    },
+]
+
+let direction = "right";
+
+
+
+////
+//// DOM ELEMENTS
+////
+
+const foodDiv = document.getElementById("food");
+const snakeHeadDiv = document.getElementById("snake-head");
+const snakeJointDivs = document.getElementsByClassName("snake-joint");
+
+
+
+////
+//// LOGIC
+////
+
+const placeFood = () => {
+    x = Math.floor(Math.random()*boardWidth);
+    y = Math.floor(Math.random()*boardHeight);
+
+    food.x = x;
+    food.y = y;
+
+    console.log("x = "+x)
+    console.log("y = "+y)
+
+    foodDiv.style.left = (x * cellSize)+"px";
+    foodDiv.style.top = (y * cellSize)+"px";
+}
+
+
+////
+//// GAME START
+////
+
+placeFood();
